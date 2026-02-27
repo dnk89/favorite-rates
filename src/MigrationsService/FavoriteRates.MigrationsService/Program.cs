2f04f9b7a68a2df554dd9eb928f2885a20b41c6b
Dmitry Kozlovsky <dmitrij.kozlovskij@gmail.com>
2026-02-27 16:00:34 +0300

    Migration service: Add project structure, dependencies, and build configuration.

---
+++ src/MigrationsService/FavoriteRates.MigrationsService/Program.cs
@@ -0,0 +1,19 @@
+﻿using FavoriteRates.UsersService.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddScoped<UsersDbContext>(sp => new UsersDbContext(
+    builder.Configuration.GetConnectionString("UsersServiceConnection")!,
+    sp.GetRequiredService<ILoggerFactory>()));
+    
+var app = builder.Build();
+
+using var scope = app.Services.CreateScope();
+
+var db = scope.ServiceProvider.GetRequiredService<UsersDbContext>();
+await db.Database.MigrateAsync();
