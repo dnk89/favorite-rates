1dec9133f4bad9603684b477fefc9cdd754a209e
Dmitry Kozlovsky <dmitrij.kozlovskij@gmail.com>
2026-03-02 10:12:58 +0300

    Finance service: Improve UserFavorites repository

--- src/FinanceService/FavoriteRates.FinanceService.Infrastructure/Persistence/Repositories/UserFavoritesRepository.cs
+++ src/FinanceService/FavoriteRates.FinanceService.Infrastructure/Persistence/Repositories/UserFavoritesRepository.cs
@@ -7,8 +7,10 @@ public class UserFavoritesRepository(FinanceDbContext dbContext) : IUserFavorite
 {
     public async Task UpdateAllAsync(UserFavorite[] userFavorites, CancellationToken cancellationToken)
     {
+        var userIds = userFavorites.Select(x => x.UserId).Distinct();
         await using var transaction = await dbContext.Database.BeginTransactionAsync(cancellationToken);
-        dbContext.UserFavorites.RemoveRange(userFavorites);
+        dbContext.UserFavorites.RemoveRange(dbContext.UserFavorites.Where(x => userIds.Contains(x.UserId)));
+        await dbContext.SaveChangesAsync(cancellationToken);
         await dbContext.UserFavorites.AddRangeAsync(userFavorites);
         await dbContext.SaveChangesAsync(cancellationToken);
         await transaction.CommitAsync(cancellationToken);
